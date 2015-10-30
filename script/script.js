f6d33590f5b5684ef294ddcc4e62e0b2921f561b
unknown <erica.gunn@gmail.com>
2015-10-30 10:10:21 -0400

    updated colors, comments

--- script/script.js
+++ script/script.js
@@ -110,22 +110,32 @@ function draw(rows, year){  //top5 array from previous function used to call thi
         .style('fill', function(d) {
             //use an accessor function to read the data stored inside the object. Then, use an if/else statement to compare
             //the contents of the d.country string with the known country names, and return a different color as a string for each one
-            if (d.country == 'Australia')           { return 'coral';  }
-            else if (d.country == 'Belgium')        { return 'orange'; }
-            else if (d.country == 'China')          { return 'yellow';  }
-            else if (d.country == 'Denmark')        { return 'lt-green'; }
-            else if (d.country == 'France')         { return 'green';  }
-            else if (d.country == 'Germany')        { return 'aqua';   }
-            else if (d.country == 'Great Britain')  { return 'blue';  }
-            else if (d.country == 'Hungary')        { return 'purple'; }
-            else if (d.country == 'Italy')          { return 'magenta'; }
-            else if (d.country == 'Japan')          { return 'brown'; }
+
+            //There are so many better ways to do this, but I was struggling with the syntax for accessing attributes stored
+            //inside joined data. I tried making a separate color array and then matching its index row by row to the countries
+            //in the array using an accessor function with (d,i). I tried pushing a new color element into the array (which,
+            //it occurs to me, I probably should have done in the parse function, rather than the draw/dataLoaded functions,
+            //which is where I tried it). I also tried storing the results of the accessor function in a variable that I could
+            //call in the .style attribute, but it returned the text of the function call as a string rather than the desired result.
+            //I thought about joining a new array to this one, but wasn't sure if you could do nested joins.
+            //In the end, I went with this one because it's functional, if inelegant.
+            //A switch statement would be another way to approach this.
+            if (d.country == 'Australia')           { return 'orange';  }
+            else if (d.country == 'Belgium')        { return 'darkorange'; }
+            else if (d.country == 'China')          { return 'tan';  }
+            else if (d.country == 'Denmark')        { return 'chocolate'; }
+            else if (d.country == 'France')         { return 'yellowGreen';  }
+            else if (d.country == 'Germany')        { return 'crimson';   }
+            else if (d.country == 'Great Britain')  { return 'darkCyan';  }
+            else if (d.country == 'Hungary')        { return 'orangeRed'; }
+            else if (d.country == 'Italy')          { return 'Peru'; }
+            else if (d.country == 'Japan')          { return 'darkOliveGreen'; }
             else if (d.country == 'Mixed team')     { return 'gray'; }
-            else if (d.country == 'Poland')         { return 'lt-purple';  }
-            else if (d.country == 'Soviet Union')   { return 'crimson';  }
-            else if (d.country == 'Switzerland')    { return 'teal';  }
-            else if (d.country == 'United States')  { return 'turquoise'; }
-            else                                    { return 'black'; }
+            else if (d.country == 'Poland')         { return 'steelBlue';  }
+            else if (d.country == 'Soviet Union')   { return 'burlyWood';  }
+            else if (d.country == 'Switzerland')    { return 'darkSeaGreen';  }
+            else if (d.country == 'United States')  { return 'lightSeaGreen'; }
+            else                                    { return 'lightSalmon'; }
           })
 
         .attr('r', 0);   //use data value stored in rows.year to set circle size
@@ -143,7 +153,7 @@ function draw(rows, year){  //top5 array from previous function used to call thi
         .attr('class', 'count')
         .attr('text-anchor', 'middle')
         .attr('transform', 'translate(0, 150)')
-        .attr('font-size','12px')
+        .attr('font-size','16px')
         .style('fill','rgb(80,80,80)');
 
     //Exit
@@ -153,6 +163,13 @@ function draw(rows, year){  //top5 array from previous function used to call thi
 
     //Update
     visualizations
+        //The notes say that you only need to call a transition once, but I wanted different timings so that the text didn't show
+        // up before the circles, and I am calling the same parent both times. Using transition twice seemed to work, but
+        // adding a third one for the labels got complicated really fast.
+
+        //Is there a way to have different transition speeds and animation delays based on class?
+        //Also, for creating a long sequence of events, how do you string transition(1, class circle).delay(1).
+        //transition(2, text).delay(2).transition(3,class circle) in order to move items around the canvas in more than one step?
         .transition(100)
         .delay(200)
         .attr("transform", function(d,i) {
